39714129d30f262bc1be7a15a4b812bedba97661
shimaa3434 <bwmn900@gmail.com>
2026-05-11 13:35:17 +0300

    Commit Message

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 
 import './App.css'
 import Todo from './Components/Todo'
+import { createContext,  useState } from "react";
+
+
+export const themeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+
+    const [themeState, setThemeState] = useState("light");
+
+    const toggle = () => {
+        setThemeState(() =>
+            themeState === "light" ? "dark" : "light"
+        );
+};
+
+    return (
+        <themeContext.Provider value={{ themeState, toggle }}>
+            {children}
+        </themeContext.Provider>
+    );
+};
 
 function App() {
-  
-
   return (
     <>
+    <ThemeProvider>
       <Todo />
+    </ThemeProvider>
+
     </>
   )
 }
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { themeContext } from "../App";
 
 export default function Todo() {
   const [task, setTask] = useState("");
   const [todos, setTodos] = useState([]);
+  const { themeState, toggle } = useContext(themeContext);
 
 
   const addTask = () => {
@@ -14,9 +16,13 @@ export default function Todo() {
   };
 
   return (
-    <div className="text-center m-auto" >
+    <div className={`container position-relative vh-100 d-flex justify-content-center align-items-center flex-column m-auto ${themeState}`}>
+      <button onClick={toggle} className={`btn btn-${themeState === "light" ? "dark" : "light"} position-absolute top-0 end-0`}>
+          {themeState === "light" ? "dark" : "light"}
+      </button>
+
       <h2 >To Do List</h2>
-      <div className="d-flex gap-3">
+      <div className="d-flex gap-3 w-75">
         <input
           type="text"
           className="form-control"
@@ -27,9 +33,9 @@ export default function Todo() {
         <button className="btn btn-danger" onClick={addTask}>Add</button>
       </div>
 
-      <ul className="list-group mt-3">
+      <ul className="list-group w-75 mt-3">
         {todos.map((todo, index) => (
-          <li key={index} className="list-group-item list-group-item-warning text-start">
+          <li key={index} className="list-group-item list-group-item-warning text-start my-1 ">
             <span className="badge bg-primary rounded-pill me-2">{index + 1}</span>
               {todo.text}
           </li>
